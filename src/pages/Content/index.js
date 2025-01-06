cf5093e9caf76b3f5f86032944427489b454f791
Shivansh Soni <shivanshsoni@berkeley.edu>
2025-01-05 18:52:13 -0800

    test

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -3,5 +3,7 @@ const panel = document.createElement("div");
 panel.id = "dummy-side-panel";
 panel.innerHTML = `<div id="panel-header">Hello World</div>`;
 
+console.log("Panel script is running");
+
 // Append the panel to the body
 document.body.appendChild(panel);
